f77e3fc5d23066e585c29292a8baeeef9471e033
Upsidedly <matthewlloydw@gmail.com>
2023-10-09 11:50:49 -0500

    fix: install script (again, again)

--- install.sh
+++ install.sh
@@ -14,8 +14,8 @@ if [ -f "$BIN_DIR/sico" ]; then
   rm "$BIN_DIR/sico"
 fi
 
-VERSION=$(sico-latest -V)
 mv sico-latest "$BIN_DIR/sico"
-echo "sico version $VERSION installed successfully!"
+chmod 755 $BIN_DIR/sico
+echo "sico version $($BIN_DIR/sico -V) installed successfully!"
 
 echo -e "put this in your ~/.bashrc:\n\nexport PATH=$BIN_DIR:\$PATH"
